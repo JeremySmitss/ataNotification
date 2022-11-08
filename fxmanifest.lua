4474a242c713aad9c62131f5f1ee84f4148fa332
JeremySmits <97687791+JeremySmitss@users.noreply.github.com>
2022-11-08 15:22:05 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+author 'YishengChew'
+description 'ataNotification'
+version '1.0'
+ui_page 'html/index.html'
+client_scripts {
+	'client.lua',
+}
+files {
+	'html/*.*',
+	'html/sound/*.*'
+}
+export 'notification'
